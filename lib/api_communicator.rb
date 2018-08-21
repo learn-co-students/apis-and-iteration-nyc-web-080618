de75a8674202012077f8e3d8170268e673918991
kristinatong <kristina.tong@gmail.com>
2018-08-21 10:43:39 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,10 +4,22 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+  people_url = "http://www.swapi.co/api/people/" #assign url
+  # response_string = RestClient.get('http://www.swapi.co/api/people/')
+  # response_hash = JSON.parse(response_string)
+  # results = response_hash["results"]
+  response_hash = get_response_hash(people_url)
+  results = response_hash["results"] #using helper method, gets response hash for characters
+  find_character = results.find do |result| #find character from results hash
+    result["name"].downcase == character
+  end
+  list_of_urls = find_character["films"] #gets film api url
+  list_of_urls.map do |url| #gets titles
+    film = get_response_hash(url)
+    film["title"]
+  end
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
@@ -22,8 +34,19 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
+def get_response_hash(url) #helper method to get hash
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+end
+
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  # titles = films_hash.map do |film|
+  #   response_string = RestClient.get(film)
+  #   response_hash = JSON.parse(response_string)
+  #   response_hash["title"]
+  # end
+  puts films_hash
 end
 
 def show_character_movies(character)
